0152b96e473560c5b1caa0692f70301005530ddd
DiegoGM98 <132071916+DiegoGM98@users.noreply.github.com>
2023-07-04 00:02:45 -0400

    Proyecto semestral

--- src/main/java/Automotora.java
+++ src/main/java/Automotora.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 class Vehiculo {
@@ -30,6 +32,9 @@ class Main {
         catalogoVehiculos.add(new Vehiculo("Toyota", "Corolla", "Rojo", 2015, 60000));
         catalogoVehiculos.add(new Vehiculo("Nissan", "Versa", "Azul", 2018, 30000));
         catalogoVehiculos.add(new Vehiculo("Honda", "Civic", "Negro", 2020, 10000));
+
+        Map<String, String> usuariosRegistrados = new HashMap<>();
+
         boolean salir = false;
         while (!salir) {
             System.out.println("1. Iniciar sesión");
@@ -38,52 +43,84 @@ class Main {
             System.out.print("Ingrese una opción: ");
             int opcion = scanner.nextInt();
             scanner.nextLine();
+
             switch (opcion) {
                 case 1:
                     System.out.print("Ingrese su RUT: ");
-                    String rut = scanner.nextLine();
+                    String rutInicioSesion = scanner.nextLine();
                     System.out.print("Ingrese su clave: ");
-                    String clave = scanner.nextLine();
-                    // Aquí puedes agregar la lógica para verificar si el RUT y la clave son correctos
-                    // y mostrar el catálogo de vehículos
-                    System.out.println("Catálogo de vehículos:");
-                    for (Vehiculo vehiculo : catalogoVehiculos) {
-                        System.out.println(vehiculo);
+                    String claveInicioSesion = scanner.nextLine();
+
+                    // Verificar si el usuario está registrado y la clave es correcta
+                    if (usuariosRegistrados.containsKey(rutInicioSesion) &&
+                            usuariosRegistrados.get(rutInicioSesion).equals(claveInicioSesion)) {
+                        System.out.println("Catálogo de vehículos:");
+                        for (Vehiculo vehiculo : catalogoVehiculos) {
+                            System.out.println(vehiculo);
+                        }
+                    } else {
+                        System.out.println("Error: RUT o clave incorrectos. No tiene acceso al catálogo.");
                     }
                     break;
+
                 case 2:
                     System.out.print("Ingrese su RUT: ");
-                    rut = scanner.nextLine();
-                    System.out.print("Ingrese su nombre: ");
-                    String nombre = scanner.nextLine();
-                    System.out.print("Ingrese su apellido paterno: ");
-                    String apellidoPaterno = scanner.nextLine();
-                    System.out.print("Ingrese su apellido materno: ");
-                    String apellidoMaterno = scanner.nextLine();
-                    int edad;
-                    do {
-                        System.out.print("Ingrese su edad (entre 18 y 75 años): ");
-                        edad = scanner.nextInt();
-                        scanner.nextLine();
-                        if (edad < 18) {
-                            System.out.println("Error: la edad debe ser mayor o igual a 18 años");
+                    String rutRegistro = scanner.nextLine();
+
+                    if (usuariosRegistrados.containsKey(rutRegistro)) {
+                        System.out.println("Error: El usuario ya está registrado.");
+                    } else if (!validarRut(rutRegistro)) {
+                        System.out.println("Error: Formato de RUT incorrecto.");
+                    } else {
+                        System.out.print("Ingrese su nombre: ");
+                        String nombre = scanner.nextLine();
+                        System.out.print("Ingrese su apellido paterno: ");
+                        String apellidoPaterno = scanner.nextLine();
+                        System.out.print("Ingrese su apellido materno: ");
+                        String apellidoMaterno = scanner.nextLine();
+                        int edad;
+                        do {
+                            System.out.print("Ingrese su edad (entre 18 y 75 años): ");
+                            edad = scanner.nextInt();
+                            scanner.nextLine();
+                            if (edad < 18) {
+                                System.out.println("Error: la edad debe ser mayor o igual a 18 años");
+                            }
+                        } while (edad < 18 || edad > 75);
+
+                        String claveRegistro;
+                        do {
+                            System.out.print("Ingrese la clave que desea (8-10 dígitos): ");
+                            claveRegistro = scanner.nextLine();
+                            if (claveRegistro.length() < 8 || claveRegistro.length() > 10) {
+                                System.out.println("Error: la clave debe tener entre 8 y 10 dígitos.");
+                            }
+                        } while (claveRegistro.length() < 8 || claveRegistro.length() > 10);
+
+                        usuariosRegistrados.put(rutRegistro, claveRegistro);
+
+                        System.out.println("Registro exitoso. Catálogo de vehículos:");
+                        for (Vehiculo vehiculo : catalogoVehiculos) {
+                            System.out.println(vehiculo);
                         }
-                    } while (edad < 18 || edad > 75);
-                    System.out.print("Ingrese la clave que desea: ");
-                    clave = scanner.nextLine();
-                    // Aquí puedes agregar la lógica para guardar la información del usuario
-                    // y mostrar el catálogo de vehículos
-                    System.out.println("Catálogo de vehículos:");
-                    for (Vehiculo vehiculo : catalogoVehiculos) {
-                        System.out.println(vehiculo);
                     }
                     break;
+
                 case 3:
                     salir = true;
                     break;
+
                 default:
                     System.out.println("Opción inválida");
             }
         }
+    }
+
+    public static boolean validarRut(String rut) {
+        // Expresión regular para validar el formato del RUT (sin puntos ni guion)
+        String rutPattern = "\\d{7,8}-[kK\\d]";
+
+        // Verificar si el RUT coincide con el patrón
+        return rut.matches(rutPattern);
     }
 }
